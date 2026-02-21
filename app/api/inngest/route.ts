8050cd0901e81343a0b5e32a60ca29f68f1423e4
Max050505 <bmaksumiv@i.ua>
2026-02-21 19:26:08 +0200

    key

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -4,7 +4,7 @@ import { sendSignUpEmail, sendDailyNewsSummary } from "@/lib/inngest/functions";
 
 export const {POST, GET, PUT} = serve({
     client: inngest,
-    signingKey: process.env.SIGNING_KEY,
+    signingKey: process.env.INNGEST_API_KEY!,
     functions: [sendSignUpEmail, sendDailyNewsSummary]
 })
 
